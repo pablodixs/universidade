88e4132a8982d1dcb479412a6d89ce3731b72564
pablodixs <pabllosoarez@gmail.com>
2024-09-19 09:20:58 -0300

    agr sim

--- src/main/java/ucb/poo/Main.java
+++ src/main/java/ucb/poo/Main.java
@@ -3,6 +3,7 @@ package ucb.poo;
 import ucb.poo.database.DB;
 
 import java.sql.*;
+import java.time.Instant;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,30 @@ public class Main {
         Statement comando = null;
         ResultSet resultado = null;
 
+        Date data = new Date(System.currentTimeMillis());
+
+        PreparedStatement statement = null;
+
         try {
             conexao = DB.conectar();
+
+            statement = conexao.prepareStatement(
+                    "INSERT INTO universidade.alunos(cpf, nome, telefone, email, data_nascimento) VALUES (?, ?, ?, ?, ?)"
+            );
+
+            statement.setString(1, "828128812");
+            statement.setString(2, "Pablo");
+            statement.setString(3, "619999999");
+            statement.setString(4, "@");
+            statement.setDate(5, data);
+
+            statement.executeUpdate();
+
+        } catch (SQLException erro) {
+            erro.printStackTrace();
+        }
+
+        try {
 
             comando = conexao.createStatement();
             resultado = comando.executeQuery("SELECT * FROM universidade.alunos");
@@ -26,22 +49,6 @@ public class Main {
             DB.fecharResultado(resultado);
             DB.fecharComando(comando);
             DB.desconectar();
-        }
-
-        PreparedStatement statement = null;
-
-        try {
-            conexao = DB.conectar();
-
-            statement = conexao.prepareStatement(
-                    "INSERT INTO universidade.alunos(cpf, nome, telefone, email, codigo_matricula) VALUES (?, ?, ?, ?, ?)"
-            );
-
-            statement.setString(1, "828128812");
-            statement.setString(2, "Pablo");
-
-        } catch (SQLException erro) {
-            erro.printStackTrace();
         }
     }
 }
